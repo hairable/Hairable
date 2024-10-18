9dbaea66b34c75d92fb802ad8b1459c472347acd
qwerrrqw <latiger95@gmail.com>
2024-10-18 10:48:34 +0900

    디버그 모드 끔

--- hairable/hairable/settings.py
+++ hairable/hairable/settings.py
@@ -25,7 +25,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = config.SECRET_KEY
 OPENAI_API_KEY = config.OPENAI_API_KEY
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ["3.38.213.199",
                 "localhost",
